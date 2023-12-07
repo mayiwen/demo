a7cbdb050c79a8c7bb305853287a22e0fb2233a9
mayiwen <i@mayiwen.com>
2023-12-07 20:24:35 +0800

    2023-12-07

--- angular/angularwithechart/src/app/app.component.ts
+++ angular/angularwithechart/src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-echarts-example';
-
+  ngOnInit() {
+    let arr = ['11:59:00', '12:00:00', '12:01:00', '12:02:00', '13:28:00', '13:29:00', '13:30:00']
+    let startIndex =arr.findIndex(item => item === '12:01:00')
+    let endIndex =arr.findIndex(item => item === '13:30:00')
+    console.log(startIndex, endIndex)
+    
+    console.log(arr.splice(startIndex, endIndex - startIndex))
+    console.log(arr)
+  }
   chartOption: any = {
     tooltip: {
       trigger: 'axis',
@@ -19,29 +27,9 @@ export class AppComponent {
       }
     },
     toolbox: {
-        feature: {
-            dataView: {
-              show: false,
-              readOnly: false,
-              title: 'Datensicht'
-            },
-            magicType: {
-              show: false,
-              type: ['line', 'bar'],
-              title: 'migic type'
-            },
-            restore: {
-              show: false,
-              title: 'Zurücksetzen'
-            },
-            saveAsImage: {
-              show: false,
-              title: 'Speichern'
-            }
-        }
     },
     legend: {
-        data: ['蒸发量', '降水量', '平均温度']
+        data: ['蒸发量', '降水量', '平均温度', '222']
     },
     xAxis: [
         {
@@ -50,26 +38,63 @@ export class AppComponent {
             axisPointer: {
                 type: 'shadow'
             }
+        },
+        {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisPointer: {
+                type: 'shadow'
+            },
+            gridIndex:1
+
         }
     ],
+    grid:[
+      {
+        left: '10%',
+        right: '8%',
+        height: '50%'
+      },
+      {
+        left: '10%',
+        right: '8%',
+        top: '63%',
+        height: '30%'
+      }
+
+    ],
+    // {
+    //   left: '10%',
+    //   right: '8%',
+    //   height: '50%'
+    // },
+    // {
+    //   left: '10%',
+    //   right: '8%',
+    //   top: '63%',
+    //   height: '16%'
+    // }
     yAxis: [
         {
             type: 'value',
             name: '水量',
-            // min: 0,
-            // max: 250,
-            // interval: 50,
             axisLabel: {
                 formatter: '{value} ml'
             },
             scale: true
         },
         {
+        
             type: 'value',
             name: '温度',
-            min: 0,
-            max: 25,
-            interval: 5,
+            axisLabel: {
+                formatter: '{value} °Css'
+            }
+        },
+        {
+          gridIndex: 1,
+            type: 'value',
+            name: '222',
             axisLabel: {
                 formatter: '{value} °C'
             }
@@ -93,9 +118,9 @@ export class AppComponent {
             yAxisIndex: 1,
             data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
             symbol: function (value: any, params: any) {
-              console.log('这是value')
-              console.log(value)
-              console.log(params);
+              // console.log('这是value')
+              // console.log(value)
+              // console.log(params);
               if (value == 2 || value == 6.2) {
                   return 'circle'; //拐点类型
               } else {
@@ -103,7 +128,24 @@ export class AppComponent {
               }
             }
 
-        }
+        },
+        // {
+        //     name: '222',
+        //     type: 'line',
+        //     yAxisIndex: 1,
+        //     data: [20, 22, 33, 45, 63, 102, 203, 234, 100, 165, 120, 62],
+        //     symbol: function (value: any, params: any) {
+        //       console.log('这是value')
+        //       console.log(value)
+        //       console.log(params);
+        //       if (value == 2 || value == 6.2) {
+        //           return 'circle'; //拐点类型
+        //       } else {
+        //           return 'none'; //拐点不显示
+        //       }
+        //     }
+
+        // }
     ]
   }
 
